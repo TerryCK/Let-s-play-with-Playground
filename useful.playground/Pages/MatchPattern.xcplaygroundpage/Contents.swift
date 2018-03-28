6836eb7845f0210e0245ccc6b7e9c65e491148a8
Terry Chen <terry.chen@xuenn.com>
2018-03-28 10:42:32 +0800

    Remove Terry's Playground

--- useful.playground/Pages/MatchPattern.xcplaygroundpage/Contents.swift
+++ useful.playground/Pages/MatchPattern.xcplaygroundpage/Contents.swift
@@ -7,12 +7,12 @@ enum Status {
 
 let status = Status.off
 
-    status ~= .on
-    status ~= .off
+status ~= .on
+status ~= .off
 //    .off ~= status wrong declaration
 
-    status == .on
-    status == .off
+status == .on
+status == .off
 
 if case .off = status {
     print()
