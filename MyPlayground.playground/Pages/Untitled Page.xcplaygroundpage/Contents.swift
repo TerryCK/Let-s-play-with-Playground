0ad844ef7ccb62abbaadab6ede8883e8b71b3463
Terry Chen <>
2018-06-01 10:31:35 +0800

    - Add Regular expression

--- MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,4 +1,19 @@
 import Foundation
-let lastLogin = Date(timeInterval: -1000, since: Date()) as NSDate
-let timeInterval = lastLogin.timeIntervalSince(Date())
+let day: Double = 24*60*60
+
+let lastLogin = Date(timeInterval: TimeInterval(-1 * day), since: Date()) as NSDate
+let nowTime = Date()
+
+let timeInterval = nowTime.timeIntervalSince(lastLogin as Date)
+print("now Time:", nowTime)
+
+print("lastLogin:", lastLogin)
+
 print(timeInterval)
+
+let ns: NSNumber = 10
+let int = Int(truncating: ns)
+
+
+var x : Int16
+
---
+++ RegularExpression.playground/Pages/RegularExpression.xcplaygroundpage/Contents.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+
+func matches(for regex: String, in text: String) -> [String] {
+    do {
+        let regex = try NSRegularExpression(pattern: regex)
+        let results = regex.matches(in: text,
+                                    range: NSRange(text.startIndex..., in: text))
+        return results.map {
+            String(text[Range($0.range, in: text)!])
+        }
+    } catch let error {
+        print("invalid regex: \(error.localizedDescription)")
+        return []
+    }
+}
+
+
+extension String {
+    func capturedGroups(withRegex pattern: String) -> [String] {
+        var results = [String]()
+        
+        var regex: NSRegularExpression
+        do {
+            regex = try NSRegularExpression(pattern: pattern, options: [])
+        } catch {
+            return results
+        }
+        
+        let matches = regex.matches(in: self, options: [], range: NSRange(location:0, length: self.count))
+        
+        guard let match = matches.first else { return results }
+        
+        let lastRangeIndex = match.numberOfRanges - 1
+        guard lastRangeIndex >= 1 else { return results }
+        
+        for i in 1...lastRangeIndex {
+            let capturedGroupIndex = match.range(at: i)
+            let matchedString = (self as NSString).substring(with: capturedGroupIndex)
+            results.append(matchedString)
+        }
+        
+        return results
+    }
+}
+// Will match "bcde"
+"abcdefg".capturedGroups(withRegex: "a(.*)f")
