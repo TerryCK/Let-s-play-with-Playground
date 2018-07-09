6db7f05ba8c8025c94bf827cd0b2b630dccddd8c
Terry Chen <terry.chen@xuenn.com>
2018-07-09 18:43:30 +0800

    - Add insertSorted

--- algorithms/Sort.playground/Contents.swift
+++ algorithms/Sort.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension Array where Element: Numeric & Comparable {
-    func bubbleSort() -> Array {
+    func bubbleSorted() -> Array {
         var result = self
         for outIndex in 0...count - 2 {
             for innerIndex in (outIndex + 1)...count - 1 where result[outIndex] > result[innerIndex] {
@@ -11,12 +11,42 @@ extension Array where Element: Numeric & Comparable {
         return result
     }
     
-    func bubbleSort(_ closure: (Element, Element) -> Bool) -> Array {
+    func bubbleSorted(by closure: (Element, Element) -> Bool) -> Array {
         var result = self
         for outIndex in 0...count - 2 {
             for innerIndex in (outIndex + 1)...count - 1 where closure(result[outIndex], result[innerIndex]) {
                 (result[outIndex], result[innerIndex]) = (result[innerIndex], result[outIndex])
             }
+            
+        }
+        return result
+    }
+    
+    
+    func insertSorted() -> Array {
+        var result = self
+        for outIndex in 2...count - 1 {
+            let key = self[outIndex]
+            var index = outIndex - 1
+            while index > 0 && result[index] > key {
+                result[index + 1] = result[index]
+                index -= 1
+            }
+            result[index + 1] = key
+        }
+        return result
+    }
+    
+    func insertSorted(by closure: (Element, Element) -> Bool) -> Array {
+        var result = self
+        for outIndex in 0...count - 1 {
+            let key = self[outIndex]
+            var index = outIndex - 1
+            while index >= 0 && closure(result[index], key) {
+                result[index + 1] = result[index]
+                index -= 1
+            }
+            result[index + 1] = key
             print(result)
         }
         return result
@@ -24,11 +54,13 @@ extension Array where Element: Numeric & Comparable {
 }
 
 
-[1,23,5,6,12].bubbleSort()
-[10.2, 20.1, 30.0, 30.1, 20.9].bubbleSort()
+[1,23,5,6,12].bubbleSorted()
+[10.2, 20.1, 30.0, 30.1, 20.9].bubbleSorted()
 
-[1,3,4,1].bubbleSort(<)
+[1,3,4,2].bubbleSorted(by: <)
 
 
+[10.2, 20.1, 30.0, 30.1, 20.9].insertSorted()
+[1,3,4,2].insertSorted(by: <)
 
 
